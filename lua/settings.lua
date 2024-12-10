034b84a65b6f6e4f75a244e48946824251e8e059
marcel <marcel@e-trailer.nl>
2024-12-10 15:49:36 +0100

    updated settings to behave correctly with spaces and tabs for different files

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,10 +1,34 @@
 
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- Special case: 2-space tabs for .proto files
+vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
+    pattern = "*.proto",
+    command = "set filetype=proto",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "proto",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+vim.opt.list = true
+vim.opt.listchars = "tab:›-,trail:·,extends:»,precedes:«,nbsp:·"
 
 vim.opt.wrap = false
 
