91e49ae43d3ca096ed102837efcda40f1ea96099
marcel <marcel@e-trailer.nl>
2024-12-18 15:24:32 +0100

    set tabs correct for makefiles

--- lua/settings.lua
+++ lua/settings.lua
@@ -27,6 +27,16 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 4
+  end,
+})
+
 vim.opt.list = true
 vim.opt.listchars = "tab:›-,trail:·,extends:»,precedes:«,nbsp:·"
 
